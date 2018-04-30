4c9f37f41e041d82413a313109a6d0daf202c747
Luc Saffre <luc.saffre@gmail.com>
2018-04-30 10:11:01 +0300

    http://luc.lino-framework.org/blog/2018/0430.html

--- eidreader/setup_info.py
+++ eidreader/setup_info.py
@@ -1,6 +1,6 @@
 SETUP_INFO = dict(
     name='eidreader',
-    version='0.0.7',
+    version='0.0.8',
     install_requires=['requests', 'PyKCS11'],
     scripts=['scripts/eidreader'],
     packages=['eidreader'],
