1a398596b7332c23b322e122ff5d86b96ab8a19b
Luc Saffre <luc.saffre@gmail.com>
2018-05-30 10:50:09 +0300

    http://luc.lino-framework.org/blog/2018/0530.html

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 fn = 'eidreader/setup_info.py'
-# import os
-# fn = os.path.join(
-#     os.path.abspath(os.path.dirname(__file__)), 'setup_info.py')
 exec(compile(open(fn, "rb").read(), fn, 'exec'))
+
+# from cx_Freeze import setup, Executable
+# SETUP_INFO.update()
+# options = {'build_exe': {'init_script':'Console'}} 
+# SETUP_INFO.update(executables=[Executable("scripts/eidreader")])
 
 if __name__ == '__main__':
     setup(**SETUP_INFO)
