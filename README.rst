31749d110991d4b8138dc407af3b63fc7bc1288b
Luc Saffre <luc.saffre@gmail.com>
2021-03-15 08:14:29 +0200

    review urls

--- README.rst
+++ README.rst
@@ -7,15 +7,13 @@ The ``eidreader`` package
 
 eidreader is a command-line script that reads data from Belgian eID cards and
 writes the data to stdout or posts it to a web server. It is designed to be used
-together  with a web application.
+together with a web application that will process the data.
 
 Not to be mixed up with its deprecated Java predecessor `eidreader
 <https://github.com/lsaffre/eidreader>`__ (same project name but
 another account).
 
-- The central project homepage is
-  http://eidreader.lino-framework.org
-- Please report issues to
-  https://github.com/lino-framework/eidreader/issues
+- The central project homepage is https://eidreader.lino-framework.org
+- Please report issues to https://github.com/lino-framework/eidreader/issues
 
 
--- eidreader/setup_info.py
+++ eidreader/setup_info.py
@@ -7,23 +7,21 @@ SETUP_INFO = dict(
     description="Read data from Belgian eId card via command-line",
     license='BSD-2-Clause',
     author='Luc Saffre',
-    url="http://eidreader.lino-framework.org",
+    url="https://eidreader.lino-framework.org",
     author_email='luc@saffre-rumma.net')
 
 SETUP_INFO.update(long_description="""\
 
 eidreader is a command-line script that reads data from Belgian eID cards and
 writes the data to stdout or posts it to a web server. It is designed to be used
-together  with a web application.
+together with a web application that will process the data.
 
 Not to be mixed up with its deprecated Java predecessor `eidreader
 <https://github.com/lsaffre/eidreader>`__ (same project name but
 another account).
 
-- The central project homepage is
-  http://eidreader.lino-framework.org
-- Please report issues to
-  https://github.com/lino-framework/eidreader/issues
+- The central project homepage is https://eidreader.lino-framework.org
+- Please report issues to https://github.com/lino-framework/eidreader/issues
 
 """)
 SETUP_INFO.update(classifiers="""\
