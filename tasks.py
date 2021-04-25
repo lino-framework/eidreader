8ecfcf6632045a1665e2f07d1cd30b7a78f0ede4
Luc Saffre <luc.saffre@gmail.com>
2021-04-25 18:51:06 +0300

    fix redirected urls

--- tasks.py
+++ tasks.py
@@ -1,8 +1,7 @@
 from atelier.invlib import setup_from_tasks
 ns = setup_from_tasks(
     globals(), 'eidreader',
-    blogref_url="http://luc.lino-framework.org",
+    blogref_url="https://luc.lino-framework.org",
     revision_control_system='git',
     # tolerate_sphinx_warnings=True,
 )
-
