0bb6a079c5829cb1f52c3843ba701490df793c55
Luc Saffre <luc.saffre@gmail.com>
2022-06-16 12:23:45 +0200

    adapt after changes in rstgen and atelier

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
+from atelier.sphinxconf import configure ; configure(globals())
 from lino.sphinxcontrib import configure ; configure(globals())
 
-from rstgen.sphinxconf import interproject
-interproject.configure(globals(), 'atelier')
+# from rstgen.sphinxconf import interproject
+# interproject.configure(globals(), 'atelier')
 
-from eidreader.setup_info import SETUP_INFO
-release = SETUP_INFO['version']
-version = '.'.join(release.split('.')[:2])
-# language = 'en'
+# from eidreader.setup_info import SETUP_INFO
+# release = SETUP_INFO['version']
+# version = '.'.join(release.split('.')[:2])
 
 project = "EIDReader"
 html_title = "EIDReader"
-copyright = '2018-2021 Rumma & Ko Ltd'
+import datetime
+copyright = '2018-{} Rumma & Ko Ltd'.format(datetime.date.today().year)
 # extensions += ['lino.sphinxcontrib.logo']
 
 html_context.update({
